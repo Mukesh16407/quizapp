a372443bf95f824aee5a091239a43d8dff409783
Mukesh16407 <mukesh.16407@gmail.com>
2022-11-30 23:55:25 +0530

    add-api-register

--- client/src/pages/common/Register/index.js
+++ client/src/pages/common/Register/index.js
@@ -2,31 +2,47 @@ import React from 'react'
 import { Form, message } from "antd";
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-
+import { registerUser } from "../../../apicalls/user";
+import { HideLoading, ShowLoading } from "../../../redux/loaderSlice";
 export const Register = () => {
 
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
     const onFinish = async (values) => {
+      try {
+        dispatch(ShowLoading());
+        const response = await registerUser(values);
+  
+        dispatch(HideLoading());
+        if (response.success) {
+          message.success(response.message);
+          navigate("/login");
+        } else {
+          message.error(response.message);
+        }
+      } catch (error) {
+        dispatch(HideLoading());
+        message.error(error.message);
+      }
     }
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-primary">
          <div className="card w-400 p-3 bg-white">
          <div className="flex flex-col">
          <h1 className="text-2xl">
-            QUIZAPP- REGISTER<i class="ri-user-add-line"></i>
+            QUIZAPP- REGISTER<i className="ri-user-add-line"></i>
           </h1>
           <div className="divider"></div>
           <Form layout="vertical" className="mt-2" onFinish={onFinish}>
             <Form.Item name="name" label="Name">
-              <input type="text"id='name' />
+              <input type="text" />
             </Form.Item>
             <Form.Item name="email" label="Email">
-              <input type="text" id='email'/>
+              <input type="text" />
             </Form.Item>
             <Form.Item name="password" label="Password">
-              <input type="password" id='password'/>
+              <input type="password" />
             </Form.Item>
             <div className="flex flex-col gap-2">
               <button
