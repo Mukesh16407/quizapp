6ac6fd3b52ed947db0f282d1881cec14f217e378
Mukesh16407 <mukesh.16407@gmail.com>
2022-12-10 09:04:19 +0530

    add-all-model

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,15 @@ import "./stylesheets/custom-components.css";
 import "./stylesheets/form-elements.css";
 import "./stylesheets/layout.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Register} from "./components/common/Register";
-import { Login } from "./components/common/Login";
+import {Register} from "./pages/common/Register/Register";
+import { Login } from "./pages/common/Login/Login";
+import { Home } from "./pages/common/Home";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register />} />
 
